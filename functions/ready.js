461edd8198bc9983425676fadb13c9f2df031b76
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2023-08-16 15:31:09 +0800

    Not Needed ❌

--- functions/ready.js
+++
@@ -1,9 +0,0 @@
-const { registerSlashCommands } = require('../../functions/registerCommand');
-
-module.exports = (client, config) => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(config.statusText, { type: 'PLAYING' });
-
-  // Call registerSlashCommands function and pass clientId for global registration
-  registerSlashCommands(config.clientId);
-};
