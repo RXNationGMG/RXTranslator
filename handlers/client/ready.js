bc83b0dcee2557be41cfd0bf59d631aa3fa7ae0f
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2023-08-16 14:58:14 +0800

    Code Cleanup 🧰

--- handlers/client/ready.js
+++ handlers/client/ready.js
@@ -1,9 +1,26 @@
 const { registerSlashCommands } = require('../../functions/registerCommand');
+const fetch = require('node-fetch');
 
-module.exports = (client, clientId, config, translations) => {
+module.exports = async (client, clientId, config, translations) => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity(config.statusText, { type: 'PLAYING' });
 
   // Call registerSlashCommands function and pass clientId and translations for global registration
   registerSlashCommands(clientId, translations);
+
+  // GitHub version check
+  const repoURL = 'https://api.github.com/repos/RXNationGMG?RXTranslator/releases/latest';
+
+  try {
+    const response = await fetch(repoURL);
+    if (response.ok) {
+      const data = await response.json();
+      const latestVersion = data.tag_name;
+      console.log(`Latest version available: ${latestVersion}`);
+    } else {
+      console.error('Error fetching GitHub repository information.');
+    }
+  } catch (error) {
+    console.error('Error fetching GitHub repository information:', error);
+  }
 };
