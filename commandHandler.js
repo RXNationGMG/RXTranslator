5602a287c22bc387084494b2e0d4a26d5efeb31a
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2023-08-16 14:48:43 +0800

    Code Cleanup 🧰

--- commandHandler.js
+++ commandHandler.js
@@ -1,14 +1,20 @@
 const { Client, Collection } = require('discord.js');
 const fs = require('fs');
 
+// Create a collection to store commands
 const commands = new Collection();
 
+/**
+ * Load commands from command files in the ./commands directory
+ */
 function loadCommands() {
   const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+  
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.set(command.data.name, command);
   }
 }
 
+// Export the commands collection and the loadCommands function
 module.exports = { commands, loadCommands };
