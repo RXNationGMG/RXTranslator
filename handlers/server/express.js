64f5163fed5b4ebaaff33db274bc0e6acfffc434
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2023-08-16 14:51:52 +0800

    Code Cleanup 🧰

--- handlers/server/express.js
+++ handlers/server/express.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+/**
+ * Start the Express server on the specified port
+ * @param {number} port - The port number to start the server on
+ */
 function startExpressServer(port) {
   app.get('/', (req, res) => {
     res.send('This Code Is Made From <a href="https://github.com/RXNationGMG">RXNationGaming</a>');
@@ -11,6 +15,10 @@ function startExpressServer(port) {
   });
 }
 
+/**
+ * Initialize and start the Express server based on the provided configuration
+ * @param {object} config - The configuration object
+ */
 module.exports = (config) => {
   if (config.expressServerEnabled) {
     startExpressServer(config.port);
